6d62050b2aeaee47b6c4fa2eb6d4a39dc38d2465
Mikko Parkkola <mikko.parkkola@iki.fi>
2026-04-17 14:52:31 +0200

    chore: sync homebrew formula to v0.13.0

--- homebrew/nowifi.rb
+++ homebrew/nowifi.rb
@@ -1,28 +1,28 @@
 class Nowifi < Formula
   desc "WiFi security assessment tool for captive portals and WPA audits"
   homepage "https://github.com/MikkoParkkola/nowifi"
-  version "0.12.0"
+  version "0.13.0"
   license "AGPL-3.0-or-later"
 
   on_macos do
     on_arm do
-      url "https://github.com/MikkoParkkola/nowifi/releases/download/v0.12.0/nowifi-darwin-arm64.tar.gz"
-      sha256 "32d4aa255f83a58885057f8ca7e65986fc61d81da05d9fee48b14a8fc1f0edf3"
+      url "https://github.com/MikkoParkkola/nowifi/releases/download/v0.13.0/nowifi-darwin-arm64.tar.gz"
+      sha256 "02bf5e6d9bf8a1f6b5cc657ba0e3dd9e89291b950f1c9f550c37408c840e1a5c"
     end
     on_intel do
-      url "https://github.com/MikkoParkkola/nowifi/releases/download/v0.12.0/nowifi-darwin-amd64.tar.gz"
-      sha256 "b86869f442ad8d0d209a25de56dc8779a711155bb5798b7ceebe73af56976900"
+      url "https://github.com/MikkoParkkola/nowifi/releases/download/v0.13.0/nowifi-darwin-amd64.tar.gz"
+      sha256 "2759db87ea640ed5016ffa0e83ec153932561c81888e5f636e0d00957bf07672"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/MikkoParkkola/nowifi/releases/download/v0.12.0/nowifi-linux-arm64.tar.gz"
-      sha256 "ad0c0a725b276ce90d794c49268faed2bb94d714d60b0f71a343530bf161669f"
+      url "https://github.com/MikkoParkkola/nowifi/releases/download/v0.13.0/nowifi-linux-arm64.tar.gz"
+      sha256 "69d2da595da15e9c960f0309f3e1cf6e9c839ee609d44d5615928747e5f53afa"
     end
     on_intel do
-      url "https://github.com/MikkoParkkola/nowifi/releases/download/v0.12.0/nowifi-linux-amd64.tar.gz"
-      sha256 "e33e205452c22fe7068e506f39400b4fe7b9011b5caea22d4141aa3ab51084bd"
+      url "https://github.com/MikkoParkkola/nowifi/releases/download/v0.13.0/nowifi-linux-amd64.tar.gz"
+      sha256 "c8b552b2aff6371bae99a6da0d37b9e1cf2a3dbe80099c1694397ea9304fd30f"
     end
   end
 
